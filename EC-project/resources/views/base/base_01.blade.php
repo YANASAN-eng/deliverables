02913320f7026228bb40c14ecaa3fb97fee16ec6
root <root@MSI>
2024-07-16 22:23:49 +0900

    [Bacchus0001]making header_01

--- EC-project/resources/views/base/base_01.blade.php
+++ EC-project/resources/views/base/base_01.blade.php
@@ -3,50 +3,61 @@
     <head>
         <meta charset="utf-8">
         <title>@yield('title')</title>
+        <!-- 独自のcss -->
+         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+         <!-- 独自のjs -->
+          <script type="text/javascript" src ="{{ asset('js/main.js') }}"></script>
+        <!-- boot strap -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
         <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <div class="d-flex align-items-center">
-                    <a class="navbar-brand" href="#"><img class="img-fluid d-inline-block" src="{{asset('img/zunda_face.jpg')}}" width="100px" height="100px"></a>
-                    <ul class="d-flex align-items-center">
-                        <li class="list-unstyled"><a class="navbar-brand" href="">ログアウト</a></li>
-                    </ul>
+        <header>
+            <nav>
+                <!-- TO DO:logo画像が入る -->
+                <a href="" id="logoBlock">
+                    <img src="{{ asset('img/zunda_face.jpg') }}" alt="" id="headerLogo" width="125" height="52">
+                </a>
+                <ul>
+                    <!-- お知らせ -->
+                    <li>
+                        <a href=""><span>お知らせ</span></a>
+                    </li>
+                    <!-- TO DO: 会社概要リンク -->
+                    <li>
+                        <div id="about" class="navWithIndent">
+                            <span>バッカススタジオについて</span>
+                            <div class="">
+                                <a href=""><span>ストーリー</apan></a>
+                                <a href=""><span>遊び方について</apan></a>
+                                <a href=""><span>お客様の声</span></a>
+                                <a href=""><span>シリーズ</span></a>
+                            </div>
+                        </div>
+                    </li>
+                    <!-- TO DO:　会社情報 -->
+                    <li>
+                        <div id="companyInfo" class="navWithIndent">
+                            <span>会社情報</span>
+                            <div class="navIndent">
+                                <a href=""><span>会社概要</span></a>
+                                <a href=""><span>目指す場所</span></a>
+                            </div>
+                        </div>
+                    </li>
+                    <li><a href=""><span></span></a></li>
+                    <li><a href=""><span></span></a></li>
+                    <li><a href=""><span></span></a></li>
+                </ul>
+                <a href=""></a>
+                <!-- hambureger menu -->
+                <div id="hamburgerBtn">
+                    <span></span>
+                    <span></span>
+                    <span></span>
                 </div>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                        </li>
-                    </ul>
-                    <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
-            </div>
-        </nav>
+            </nav>
+        </header>
     </body>
 </html>
